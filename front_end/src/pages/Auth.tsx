a045075e59e7cdc1d7e4b18a6d733fea655fc03b
Switer <hungtranpc1342l6@gmail.com>
2025-09-19 21:44:49 +0700

    Fix success styles of Auth.page

--- front_end/src/pages/Auth.tsx
+++ front_end/src/pages/Auth.tsx
@@ -1,71 +1,71 @@
 import { useState } from "react";
-import "../assets/styles/Auth.module.css";
+import styles from "../assets/styles/Auth.module.css"; 
 
 const Auth = () => {
     const [isActive, setActive] = useState(false);
 
 
     return (
-        <div className="auth-container">
-            <div className= {`container-auth ${isActive ? "active" : ""}`}>
-                <div className="form-box login">
+        <div className={styles["auth-wrapper"]}>
+            <div className= {`${styles["container-auth"]} ${isActive ? `${styles["active"]}` : ""}`}>
+                <div className={`${styles["form-box"]} ${styles["login"]}`}>
                     <form action="">
                         <h1>Login</h1>
-                        <div className="input-box">
+                        <div className={styles["input-box"]}>
                             <input type="text" placeholder="Username" required/>
                             <i className="ri-user-fill"></i>
                         </div>
-                        <div className="input-box">
+                        <div className={styles["input-box"]}>
                             <input type="password" placeholder="Password" required/>
                             <i className="ri-lock-2-fill"></i>
                         </div>
-                        <div className="forgot-link">
+                        <div className={styles["forgot-link"]}>
                             <a href="/">Forgot Password?</a>
                         </div>
-                        <button type="submit" className="btn">Login</button>
+                        <button type="submit" className={styles["btn"]}>Login</button>
                         <p>or login with social platforms</p>
-                        <div className="social-icons">
+                        <div className={styles["social-icons"]}>
                             <a href="/"><i className="ri-google-fill"></i></a>
                             <a href="/"><i className="ri-facebook-fill"></i></a>
                         </div>
                     </form>
                 </div>
 
-                <div className="form-box register">
+                <div className={`${styles["form-box"]} ${styles["register"]}`}>
                     <form action="">
                         <h1>Registration</h1>
-                        <div className="input-box">
+                        <div className={styles["input-box"]}>
                             <input type="text" placeholder="Username" required/>
                             <i className="ri-user-fill"></i>
                         </div>
-                        <div className="input-box">
+                        <div className={styles["input-box"]}>
                             <input type="email" placeholder="Email" required/>
                             <i className="ri-mail-fill"></i>
                         </div>
-                        <div className="input-box">
+                        <div className={styles["input-box"]}>
                             <input type="password" placeholder="Password" required/>
                             <i className="ri-lock-2-fill"></i>
                         </div>
-                        <button type="submit" className="btn">Register</button>
+                        <button type="submit" className={styles["btn"]}>Register</button>
                         <p>or register with social platforms</p>
-                        <div className="social-icons">
+                        <div className={styles["social-icons"]}>
                             <a href="/"><i className="ri-google-fill"></i></a>
                             <a href="/"><i className="ri-facebook-fill"></i></a>
                         </div>
                     </form>
                 </div>
 
-                <div className="toggle-box">
-                    <div className="toggle-panel toggle-left">
+                <div className={styles["toggle-box"]}>
+                    <div className={`${styles["toggle-panel"]} ${styles["toggle-left"]}`}>
                         <h1>Hello, Welcome!</h1>
                         <p>Don't have an account</p>
-                        <button className="btn btn-register" onClick={() => {setActive(true)}}>Register</button>
+                        <button className={`${styles["btn"]} ${styles["btn-register"]}`} onClick={() => {setActive(true)}}>Register</button>
                     </div>
 
-                    <div className="toggle-panel toggle-right">
+                    <div className={`${styles["toggle-panel"]} ${styles["toggle-right"]}`}>
                         <h1>Welcome Back!</h1>
                         <p>Already have an account?</p>
-                        <button className="btn btn-login" onClick={() => setActive(false)}>Login</button>
+                        <button className={`${styles["btn"]} ${styles["btn-login"]}`} onClick={() => setActive(false)}>Login</button>
                     </div>
                 </div>
             </div>
